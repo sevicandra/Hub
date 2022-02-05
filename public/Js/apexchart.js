c2ebeedc12eed6ebb82cf55c5c7731012727a4a8
DESKTOP-P7TP8KO\sevic <sevicandra4@gmail.com>
2022-02-05 20:55:53 +0900

    Memperbaiki UI Halaman Dashboard

--- public/Js/apexchart.js
+++ public/Js/apexchart.js
@@ -1,42 +1,42 @@
 
 var options = {
     chart: {
-        height: 350,
-        
+        height: 328,
+        width:270,
         type: 'radialBar',
     },
     series: [70],
-    labels: ['Pengelolaan Kekayaan Negara'],
+    labels: ['Kekayaan Negara'],
   }
   
   var chart1 = new ApexCharts(document.querySelector("#PNBP1"), options);
   
   chart1.render();
 
-  var options = {
+  var options1 = {
     chart: {
-        height: 350,
-        
+        height: 328,
+        width:270,
         type: 'radialBar',
     },
     series: [70],
     labels: ['Lelang'],
   }
   
-  var chart2 = new ApexCharts(document.querySelector("#PNBP2"), options);
+  var chart2 = new ApexCharts(document.querySelector("#PNBP2"), options1);
   
   chart2.render();
 
-  var options = {
+  var options2 = {
     chart: {
-        height: 350,
-        
+        height: 328,
+        width:270,
         type: 'radialBar',
     },
     series: [50],
     labels: ['Piutang Negara'],
   }
   
-  var chart3 = new ApexCharts(document.querySelector("#PNBP3"), options);
+  var chart3 = new ApexCharts(document.querySelector("#PNBP3"), options2);
   
   chart3.render();
